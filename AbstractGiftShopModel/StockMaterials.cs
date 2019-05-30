d9c27a684e93d0dcc010696ba92d4c845ed8cb16
Toshi <43044202+Akkitoshi@users.noreply.github.com>
2019-05-30 13:28:37 +0400

    5labfix

--- AbstractGiftShopModel/StockMaterials.cs
+++ AbstractGiftShopModel/StockMaterials.cs
@@ -1,4 +1,6 @@
-﻿namespace AbstractGiftShopModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbstractGiftShopModel
 {
     /// <summary>
     /// Сколько материалов хранится на складе
@@ -9,5 +11,7 @@
         public int SStockId { get; set; }
         public int MaterialsId { get; set; }
         public int Count { get; set; }
+        public virtual Materials Materials { get; set; }
+        public virtual SStock SStock { get; set; }
     }
-}
+}
