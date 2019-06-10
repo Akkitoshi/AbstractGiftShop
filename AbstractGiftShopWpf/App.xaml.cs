96f06c199baf2e0f57c6d02e65096c165ba6c16e
Akkitoshi <mikotottoro@gmail.ru>
2019-06-10 18:58:15 +0400

    WPF_Lab2.1

--- AbstractGiftShopWpf/App.xaml.cs
+++ AbstractGiftShopWpf/App.xaml.cs
@@ -28,26 +28,4 @@ namespace AbstractGiftShopWPF
 
     }
 }
-       /* App()
-        {
-            InitializeComponent();
-        }
-        [STAThread]
-       static void Main()
-        {
-            App app = new App();
-            var container = BuildUnityContainer();
-            app.Run(container.Resolve<MainWindow>());
-        }
-        public static IUnityContainer BuildUnityContainer()
-        {
-            var currentContainer = new UnityContainer();
-            IUnityContainer container = new UnityContainer();
-            currentContainer.RegisterType<ISClientService, ClientServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IGiftService, GiftServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IMaterialsService, MaterialsServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IMainService, MainServiceList>(new HierarchicalLifetimeManager());
-            return currentContainer;
-        }
-    }
-}*/
+       
