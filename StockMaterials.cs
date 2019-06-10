be14f5d3c5b525f8b7e552ee40173bd97047fbd7
Toshi <43044202+Akkitoshi@users.noreply.github.com>
2019-06-10 19:45:39 +0400

    Lab8

---
+++ StockMaterials.cs
@@ -0,0 +1,16 @@
+﻿namespace AbstractGiftShopModel
+{
+    /// <summary>
+    /// Сколько материалов хранится на складе
+    /// </summary>
+    public class StockMaterials
+    {
+        public int Id { get; set; }
+        public int SStockId { get; set; }
+        public int MaterialsId { get; set; }
+        public int Count { get; set; }
+        public virtual SStock SStock { get; set; }
+        public virtual Materials Materials { get; set; }
+
+    }
+}
